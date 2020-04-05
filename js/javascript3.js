93182c32cf5f83613bad802716eb506a754b0ac1
Lei <gabriel_sl19798@hotmail.com>
2020-04-06 09:16:05 +1200

    explained .

---
+++ js/javascript3.js
@@ -0,0 +1,11 @@
+function changeParaText() {
+    //after . you will see attributes and methods of this object
+    //. means mine, his, her, your
+    // document is my file
+    // getElementById is one method belongs to document
+    targetPara = document.getElementById("mypara");
+    //my paragraph is an object
+    //my paragraph has attribute innerText
+    targetPara.innerText = "Hello World";
+}
+
