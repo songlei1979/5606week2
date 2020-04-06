4769a07af2561235d523d82046c03221fff86b81
Lei <gabriel_sl19798@hotmail.com>
2020-04-07 10:30:41 +1200

    calculation operations

--- js/javascript4.js
+++ js/javascript4.js
@@ -1,5 +1,19 @@
 function showString() {
-    myString = "Hello World"; //everything in between " " is string
+    myInput = document.getElementById("userMessage");
+    myString = myInput.value;
+    // myString = "Hello World"; //everything in between " " is string
     myPara=document.getElementById("stringExample");
     myPara.innerText = myString;
+}
+
+function calculate() {
+    num1Input = document.getElementById("num1Input");
+    num2Input = document.getElementById("num2Input");
+    var num1 = num1Input.value; //var means define the after word to be a variable, in one file, you only do this once
+    var num2 = num2Input.value; // parseFloat will convert your string to double
+    var result = num1+num2;
+    //result = num1 * num2;
+    var resultPara = document.getElementById("showCalculationResult");
+    resultPara.innerText=result;
+
 }
