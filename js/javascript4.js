1ea5463c572bd181f7aeeb5b09cdfecf9fecc678
Lei <gabriel_sl19798@hotmail.com>
2020-04-08 10:56:46 +1200

    condition if else | if else if else | switch case

--- js/javascript4.js
+++ js/javascript4.js
@@ -9,8 +9,8 @@ function showString() {
 function calculate() {
     num1Input = document.getElementById("num1Input");
     num2Input = document.getElementById("num2Input");
-    var num1 = num1Input.value; //var means define the after word to be a variable, in one file, you only do this once
-    var num2 = num2Input.value; // parseFloat will convert your string to double
+    var num1 = parseFloat(num1Input.value); //var means define the after word to be a variable, in one file, you only do this once
+    var num2 = parseFloat(num2Input.value); // parseFloat will convert your string to double
     var result = num1+num2;
     //result = num1 * num2;
     var resultPara = document.getElementById("showCalculationResult");
